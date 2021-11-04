86194b271c372a3bad42e0c01d0c6f0b47700011
Brandon Main <brandonlmain@gmail.com>
2021-11-04 15:31:04 -0400

    update migrations

---
+++ lims_server/Migrations/20211104191513_AddLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LimsServer.Migrations
+{
+    public partial class AddLog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    workflowId = table.Column<string>(nullable: true),
+                    taskId = table.Column<string>(nullable: true),
+                    taskHangfireID = table.Column<string>(nullable: true),
+                    processorId = table.Column<string>(nullable: true),
+                    message = table.Column<string>(nullable: true),
+                    type = table.Column<string>(nullable: true),
+                    time = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}
