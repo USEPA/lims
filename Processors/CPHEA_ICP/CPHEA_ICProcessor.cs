56aebbe3fada1ed39d6ce8200ea2e999221836ea
kurtw555 <kurt.wolfe@gmail.com>
2022-01-10 17:23:55 -0500

    Add new processor: CPHEA_ICP

---
+++ Processors/CPHEA_ICP/CPHEA_ICProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PluginBase;
+using System.Reflection;
+using System.IO;
+using System.Data;
+
+namespace CPHEA_ICP
+{
+    public class CPHEA_ICProcessor : DataProcessor
+    {
+        public override string id { get => "cphea_ic_version1.0"; }
+        public override string name { get => "CPHEA_IC"; }
+        public override string description { get => "Processor used for CPHEA IC translation to universal template"; }
+        public override string file_type { get => ".csv"; }
+        public override string version { get => "1.0"; }
+        public override string input_file { get; set; }
+        public override string path { get; set; }
+
+        public CPHEA_ICProcessor()
+        {
+        }
+
+        public override DataTableResponseMessage Execute()
+        {
+            DataTableResponseMessage rm = new DataTableResponseMessage();
+            DataTable dt = GetDataTable();
+            dt.TableName = System.IO.Path.GetFileNameWithoutExtension(input_file);
+
+
+            return rm;
+        }
+    }
+}
