5cdd94cedaab89510d763da29dd908e04bccfd25
Jason Dunken <jasondunken@yahoo.com>
2022-03-07 11:21:04 -0500

    implement enable/disable processor

--- lims_angular/src/app/admin/processors/processors.component.ts
+++ lims_angular/src/app/admin/processors/processors.component.ts
@@ -6,53 +6,65 @@ import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
-  selector: "app-processors",
-  templateUrl: "./processors.component.html",
-  styleUrls: ["./processors.component.css"],
+    selector: "app-processors",
+    templateUrl: "./processors.component.html",
+    styleUrls: ["./processors.component.css"],
 })
 export class ProcessorsComponent implements OnInit {
-  loadingProcessors: boolean;
-  statusMessage: string;
-  addingProcessor: boolean;
+    loadingProcessors: boolean;
+    statusMessage: string;
+    addingProcessor: boolean;
 
-  columnNames = ["name", "description", "file_type", "processor_status"];
-  processors: Processor[];
-  sortableData = new MatTableDataSource();
+    columnNames = ["name", "description", "file_type", "processor_status"];
+    processors: Processor[];
+    sortableData = new MatTableDataSource();
 
-  constructor(private fileMgr: TaskManagerService) {}
+    constructor(private taskMgr: TaskManagerService) {}
 
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  ngOnInit() {
-    this.loadingProcessors = true;
-    this.statusMessage = "";
-    this.processors = [];
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
+    ngOnInit() {
+        this.loadingProcessors = true;
+        this.statusMessage = "";
+        this.processors = [];
 
-    this.fileMgr.getProcessors().subscribe(
-      (processors) => {
-        if (processors.error) {
-          this.statusMessage = processors.error;
+        this.getProcessors();
+    }
+
+    getProcessors(): void {
+        this.taskMgr.getProcessors().subscribe(
+            (processors) => {
+                if (processors.error) {
+                    this.statusMessage = processors.error;
+                } else {
+                    if (processors && processors.length > 0) {
+                        this.processors = [...processors];
+                        this.sortableData.data = [...this.processors];
+                        this.sortableData.sort = this.sort;
+                        this.statusMessage = "";
+                        console.log("processors: ", this.processors);
+                    } else {
+                        this.statusMessage = "There are currently no Processors installed";
+                    }
+                }
+            },
+            (err) => {
+                this.statusMessage = "Error retrieving data";
+            },
+            () => {
+                this.loadingProcessors = false;
+            }
+        );
+    }
+
+    toggleEnable(processor): void {
+        if (processor.enabled) {
+            this.taskMgr.disableProcessor(processor).subscribe(() => {
+                this.getProcessors();
+            });
         } else {
-          if (processors && processors.length > 0) {
-            this.processors = [...processors];
-            this.sortableData.data = [...this.processors];
-            this.sortableData.sort = this.sort;
-            this.statusMessage = "";
-            console.log("processors: ", this.processors);
-          } else {
-            this.statusMessage = "There are currently no Processors installed";
-          }
+            this.taskMgr.disableProcessor(processor).subscribe(() => {
+                this.getProcessors();
+            });
         }
-      },
-      (err) => {
-        this.statusMessage = "Error retrieving data";
-      },
-      () => {
-        this.loadingProcessors = false;
-      }
-    );
-  }
-
-  toggleEnable(): void {
-    console.log("toggle enable");
-  }
+    }
 }
