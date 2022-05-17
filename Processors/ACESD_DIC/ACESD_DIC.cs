587ae2074dda2884f500e247501b64aead99deac
kurtw555 <kurt.wolfe@gmail.com>
2022-05-17 14:06:57 -0400

    Finish ACESD DIC and DOC processors

--- Processors/ACESD_DIC/ACESD_DIC.cs
+++ Processors/ACESD_DIC/ACESD_DIC.cs
@@ -7,9 +7,9 @@ using PluginBase;
 //using OfficeOpenXml;
 
 
-namespace ACESD_DIC
+namespace ACESD_DOC
 {
-    public class ACESD_DIC : DataProcessor
+    public class ACESD_DOC : DataProcessor
     {
 
         public override string id { get => "acesd_dic_version1.0"; }
@@ -46,8 +46,8 @@ namespace ACESD_DIC
                     while ((line = sr.ReadLine()) != null)
                     {
                         current_row = idxRow;
-                        //First row is header. We'll skip it
-                        if (idxRow < 14)
+                        //Data starts in row 12
+                        if (idxRow < 12)
                         {
                             idxRow++;
                             continue;
@@ -62,7 +62,7 @@ namespace ACESD_DIC
                         string[] tokens = line.Split("\t");
 
                         //Analyte ID
-                        string analyteTmp = tokens[1].Trim();
+                        string analyteTmp = tokens[ColumnIndex0.B].Trim();
                         string analyteID = "";
                         if (string.Compare(analyteTmp, "IC", true) == 0)
                             analyteID = "DIC";
@@ -71,33 +71,33 @@ namespace ACESD_DIC
                         else //Analyte ID in file is not IC or NPOC
                             throw new Exception(String.Format("File: {0} - Analyte ID is not IC or NPOC. column B, row {1}: {2}", input_file, idxRow, analyteTmp));
 
-                        //Aliquot
-                        //Aliquot in column C - some rows are string some are numbers
-                        //KW - 2021-11-29 - Change this to column D. Reference email from Jakob Fox on 2021-11-29
-                        string aliquot = tokens[3].Trim();
+                        //Aliquot                        
+                        string aliquot = tokens[ColumnIndex0.C].Trim();
 
                         //Analysis DateTime
-                        //Date time in column I - e.g. 5/27/2021 14:25
+                        //Date time in column M - e.g. 5/27/2021 14:25
                         DateTime analysisDateTime;
-                        if (!DateTime.TryParse(tokens[8].Trim(), out analysisDateTime))
+                        if (!DateTime.TryParse(tokens[ColumnIndex0.M].Trim(), out analysisDateTime))
                             throw new Exception(String.Format("File: {0} - Analysis DateTime is not valid. Row {1}", input_file, idxRow));
 
                         //Maps to column N
-                        string measuredValTmp = tokens[13].Trim();
+                        string measuredValTmp = tokens[ColumnIndex0.G].Trim();
                         double measuredVal;
                         if (!double.TryParse(measuredValTmp, out measuredVal))
                             throw new Exception("Invalid measured value in row: " + idxRow.ToString());
+
+                        units = tokens[ColumnIndex0.K].Trim();
 
                         DataRow dr = dt.NewRow();
                         dr["Aliquot"] = aliquot;
                         dr["Analyte Identifier"] = analyteID;
                         dr["Measured Value"] = measuredVal;
-                        //dr[3] = units;
+                        dr["Units"] = units;
                         dr["Analysis Date/Time"] = analysisDateTime;
                         dt.Rows.Add(dr);
 
                     }
-                }                                       
+                }
             }
             catch (Exception ex)
             {
