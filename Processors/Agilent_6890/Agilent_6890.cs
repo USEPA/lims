0bfd3379d0658af57ff8dae27650869936e7c5a7
kurtw555 <kurt.wolfe@gmail.com>
2022-06-14 17:21:41 -0400

    Added new project

---
+++ Processors/Agilent_6890/Agilent_6890.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Text.RegularExpressions;
+using PluginBase;
+using System.Globalization;
+
+namespace Agilent_6890
+{
+    public class Agilent_6890 : DataProcessor
+    {
+        public override string id { get => "agilent_6890.0"; }
+        public override string name { get => "Agilent_6890"; }
+        public override string description { get => "Processor used for Agilent_6890 translation to universal template"; }
+        public override string file_type { get => ".txt"; }
+        public override string version { get => "1.0"; }
+        public override string? input_file { get; set; }
+        public override string? path { get; set; }
+
+        public override DataTableResponseMessage Execute()
+        {
+            DataTableResponseMessage rm = null;
+            DataTable? dt = null;
+            try
+            {
+                rm = VerifyInputFile();
+                if (!rm.IsValid)
+                    return rm;
+
+                dt = GetDataTable();
+                FileInfo fi = new FileInfo(input_file);
+                dt.TableName = System.IO.Path.GetFileNameWithoutExtension(fi.FullName);
+
+                using StreamReader sr = new StreamReader(input_file);
+                string? line;
+
+                string[] tokens;
+                int rowIdx = 0;
+                //bool bStart = false;
+                bool bDataFile = false;
+                bool bQuantTime = false;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    rowIdx++;
+                    current_row = rowIdx;
+                    string currentLine = line;
+                }
+            }
+            catch (Exception ex)
+            {
+                string errorMsg = string.Format("Problem executing processor {0} on input file {1}.", name, input_file);
+                errorMsg = errorMsg + Environment.NewLine;
+                errorMsg = errorMsg + ex.Message;
+                errorMsg = errorMsg + Environment.NewLine;
+                errorMsg = errorMsg + string.Format("Error occurred on row: {0}", current_row + 1);
+                rm.ErrorMessage = errorMsg;
+            }
+
+            rm.TemplateData = dt;
+
+            return rm;
+        }
+    }
+}
