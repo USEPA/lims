0a90a64040e1a6970ff88d88a48d91b57ae7eb1e
kurtw555 <kurt.wolfe@gmail.com>
2022-09-26 16:48:28 -0400

    Initial upload of EGB YSI6600 parser

---
+++ Processors/EGB_YSI6600/EGB_YSI6600.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using PluginBase;
+using OfficeOpenXml;
+
+namespace EGB_YSI6600
+{
+    public class EGB_YSI6600 : DataProcessor
+    {
+        public override string id { get => "egb_ysi66001.0"; }
+        public override string name { get => "EGB_YSI6600"; }
+        public override string description { get => "Processor used for EGB_YSI6600 translation to universal template"; }
+        public override string file_type { get => ".xlsx"; }
+        public override string version { get => "1.0"; }
+        public override string input_file { get; set; }
+        public override string path { get; set; }
+
+        public override DataTableResponseMessage Execute()
+        {
+            DataTableResponseMessage rm = null;
+            DataTable dt = null;
+            try
+            {
+                rm = VerifyInputFile();
+                if (rm.IsValid == false)
+                    return rm;
+
+                dt = GetDataTable();
+                FileInfo fi = new FileInfo(input_file);
+                dt.TableName = System.IO.Path.GetFileNameWithoutExtension(fi.FullName);
+
+                //New in version 5 - must deal with License
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                //This is a new way of using the 'using' keyword with braces
+                using var package = new ExcelPackage(fi);
+
+                var worksheet = package.Workbook.Worksheets[0];  //Worksheets are zero-based index                
+                string name = worksheet.Name;
+
+                //File validation
+                if (worksheet.Dimension == null)
+                {
+                    string msg = string.Format("No data in Sheet 1 in InputFile:  {0}", input_file);
+                    rm.LogMessage = msg;
+                    rm.ErrorMessage = msg;
+                    return rm;
+                }
+
+                int startRow = worksheet.Dimension.Start.Row;
+                int startCol = worksheet.Dimension.Start.Column;
+                int numRows = worksheet.Dimension.End.Row;
+                int numCols = worksheet.Dimension.End.Column;
+
+
+                
+                string user_defined1 = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, 6]));
+                
+                string analyteID = "";
+
+                for (int rowIdx = 7; rowIdx < numRows; rowIdx++)
+                {
+                }
+
+            }
+            catch (Exception ex)
+            {
+                rm.LogMessage = string.Format("Processor: {0},  InputFile: {1}, Exception: {2}", name, input_file, ex.Message);
+                rm.ErrorMessage = string.Format("Problem executing processor {0} on input file {1}.", name, input_file);
+            }
+
+            rm.TemplateData = dt;
+
+            return rm;
+
+        }
+    }
+}
