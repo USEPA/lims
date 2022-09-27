8d9f02cc867416cbaaa3f002dfcf93e72ebd50cb
kurtw555 <kurt.wolfe@gmail.com>
2022-09-27 11:34:56 -0400

    More progress on EGB YSI6600 parser

--- Processors/EGB_YSI6600/EGB_YSI6600.cs
+++ Processors/EGB_YSI6600/EGB_YSI6600.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.IO;
 using PluginBase;
 using OfficeOpenXml;
+using System.Collections.Generic;
 
 namespace EGB_YSI6600
 {
@@ -51,8 +52,7 @@ namespace EGB_YSI6600
                 int startCol = worksheet.Dimension.Start.Column;
                 int numRows = worksheet.Dimension.End.Row;
                 int numCols = worksheet.Dimension.End.Column;
-                
-                string analyteID_J = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.J]));
+                                
                 string analyteID_K = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.K]));
                 string analyteID_L = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.L]));
                 string analyteID_M = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.M]));
@@ -63,19 +63,38 @@ namespace EGB_YSI6600
                 string analyteID_R = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.R]));
                 string analyteID_S = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.S]));
                 string analyteID_T = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.T]));
+                string analyteID_U = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.U]));
 
                 aliquot = "";
+                Dictionary<string, List<double>> dctMeasuredVals = new Dictionary<string, List<double>>(11);
+                Dictionary<string, int> dctCount = new Dictionary<string, int>();
                 string currentAliquot = "";
                 for (int rowIdx = 3; rowIdx < numRows; rowIdx++)
                 {
                     current_row = rowIdx;
                     currentAliquot = GetXLStringValue(worksheet.Cells[rowIdx, ColumnIndex1.A]);
+                    if (!dctMeasuredVals.ContainsKey(currentAliquot))
+                        dctMeasuredVals.Add(currentAliquot, new List<double>(11));
+                   
                     string sDate = GetXLStringValue(worksheet.Cells[rowIdx, ColumnIndex1.B]);
                     string sTime = GetXLStringValue(worksheet.Cells[rowIdx, ColumnIndex1.C]);
                     if (!DateTime.TryParse(sDate + " " + sTime, out analysisDateTime))
                         throw  new Exception($"Invalid Analysis DateTime: {sDate} {sTime}");
 
                     userDefined1 = GetXLStringValue(worksheet.Cells[rowIdx, ColumnIndex1.I]);
+
+                    List<double> lstTmp = dctMeasuredVals[currentAliquot];
+
+                    //Keep track of how many aliquots need to be averaged
+                    dctCount[currentAliquot] += 1;
+                    int lstIdx = 0;
+                    for (int colIdx = ColumnIndex1.K; colIdx <= ColumnIndex1.U; colIdx++)
+                    {
+                        double dval = GetXLDoubleValue(worksheet.Cells[rowIdx, colIdx]);
+                        lstTmp[lstIdx] += dval;
+                        lstIdx++;
+                    }
+                    
 
                 }
 
