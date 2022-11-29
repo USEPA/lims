d777e3938c4146842468be170d5e8b7692529eac
jasondunken <jasondunken@yahoo.com>
2022-11-29 08:39:29 -0500

    account for 0th hour

--- Processors/ACESD_Fecal_Coliform/ACESD_Fecal_Coliform.cs
+++ Processors/ACESD_Fecal_Coliform/ACESD_Fecal_Coliform.cs
@@ -223,6 +223,10 @@ namespace ACESD_Fecal_Coliform
             {
                 meridiem = "PM";
             }
+            if (hour == 0)
+            {
+                hour = 12;
+            }
             if (hour > 12)
             {
                 hour -= 12;
