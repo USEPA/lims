6eb3f94051d238886e2955c8d84d9ea8369dc3b9
kurtw555 <kurt.wolfe@gmail.com>
2023-02-08 14:14:01 -0500

    Update to EGB YSI16600 parser

--- Processors/EGB_YSI6600/EGB_YSI6600.cs
+++ Processors/EGB_YSI6600/EGB_YSI6600.cs
@@ -59,7 +59,7 @@ namespace EGB_YSI6600
                 int numCols = worksheet.Dimension.End.Column;
                                 
                 List<string> lstAnalyteIDs = new List<string>();
-                for (int i = ColumnIndex1.K; i <= ColumnIndex1.U; i++)                
+                for (int i = ColumnIndex1.L; i <= ColumnIndex1.V; i++)                
                     lstAnalyteIDs.Add(GetXLStringValue(worksheet.Cells[2, i]));
                 
                 //string analyteID_K = Convert.ToString(GetXLDoubleValue(worksheet.Cells[2, ColumnIndex1.K]));
@@ -78,7 +78,7 @@ namespace EGB_YSI6600
                 Dictionary<string, int> dctCount = new Dictionary<string, int>();
                 string currentAliquot = "";
                 AliquotData aliquot = null;
-                for (int rowIdx = 3; rowIdx <= numRows; rowIdx++)
+                for (int rowIdx = 2; rowIdx <= numRows; rowIdx++)
                 {
                     current_row = rowIdx;
                     //If this value is 'QAC' then skip it
@@ -103,7 +103,7 @@ namespace EGB_YSI6600
                         //    throw new Exception($"Invalid Analysis DateTime: {sDate} {sTime}");
                         aliquot.AnalysisDateTime = date_time;
 
-                        userDefined1 = GetXLStringValue(worksheet.Cells[rowIdx, ColumnIndex1.I]);
+                        userDefined1 = GetXLStringValue(worksheet.Cells[rowIdx, ColumnIndex1.X]);
                         aliquot.UserDefined1 = userDefined1;
                     }
                     else
@@ -112,7 +112,7 @@ namespace EGB_YSI6600
                     //Keep track of how many aliquots need to be averaged
                     aliquot.Count += 1;
                     int lstIdx = 0;
-                    for (int colIdx = ColumnIndex1.K; colIdx <= ColumnIndex1.U; colIdx++)
+                    for (int colIdx = ColumnIndex1.L; colIdx <= ColumnIndex1.V; colIdx++)
                     {
                         double dval = GetXLDoubleValue(worksheet.Cells[rowIdx, colIdx]);
                         aliquot.MeasuredValues[lstIdx] += dval;
