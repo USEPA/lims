1759bf9cdad2486438037d6013fe33adee61e761
kurtw555 <kurt.wolfe@gmail.com>
2023-02-28 16:56:39 -0500

    Intial commit for IRSB ICP OES

---
+++ Processors/IRSB_ICP_OES/IRSB_ICP_OES.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using PluginBase;
+using OfficeOpenXml;
+
+
+namespace IRSB_ICP_OES
+{
+    public class IRSB_ICP_OES : DataProcessor
+    {
+        public override string id { get => "irsb_icp_oes"; }
+        public override string name { get => "IRSB_ICP_OES"; }
+        public override string description { get => "Processor used for IRSB_ICP_OES translation to universal template"; }
+        public override string file_type { get => ".xlsx"; }
+        public override string version { get => "1.0"; }
+        public override string input_file { get; set; }
+        public override string path { get; set; }
+
+        public override DataTableResponseMessage Execute()
+        {
+            DataTableResponseMessage rm = null;
+            DataTable dt = null;
+            try
+            {
+                rm = VerifyInputFile();
+                if (!rm.IsValid)
+                    return rm;
+
+                dt = GetDataTable();
+                FileInfo fi = new FileInfo(input_file);
+                dt.TableName = System.IO.Path.GetFileNameWithoutExtension(fi.FullName);
+
+                //New in version 5 - must deal with License
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                //This is a new way of using the 'using' keyword with braces
+                using var package = new ExcelPackage(fi);
+
+                var worksheet = package.Workbook.Worksheets[0];  //Worksheets are zero-based index                
+                string name = worksheet.Name;
+
+                //File validation
+                if (worksheet.Dimension == null)
+                    throw new Exception("Spreadsheet contains no data");
+
+                int startRow = worksheet.Dimension.Start.Row;
+                int startCol = worksheet.Dimension.Start.Column;
+                int numRows = worksheet.Dimension.End.Row;
+                int numCols = worksheet.Dimension.End.Column;
+
+            }
+            catch (Exception ex)
+            {
+                string errorMsg = string.Format("Problem executing processor {0} on input file {1}.", name, input_file);
+                errorMsg = errorMsg + Environment.NewLine;
+                errorMsg = errorMsg + ex.Message;
+                errorMsg = errorMsg + Environment.NewLine;
+                errorMsg = errorMsg + string.Format("Error occurred on row: {0}", current_row);
+                rm.ErrorMessage = errorMsg;
+            }
+            rm.TemplateData = dt;
+            return rm;
+        }
+    }
+}
