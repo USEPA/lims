c0b8a14c78d9bdedfa11904f195b940ab3bde6a6
popenc <i.nickpope@gmail.com>
2024-02-07 11:01:39 -0500

    added banner to header component

--- lims_angular/src/app/components/header/header.component.ts
+++ lims_angular/src/app/components/header/header.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Subscription } from 'rxjs';
 
 import { AuthService } from "../../services/auth.service";
 import { TaskManagerService } from "../../services/task-manager.service";
+import { EnvService } from "../../services/env.service";
+
+
 
 @Component({
     selector: "app-header",
@@ -10,12 +14,25 @@ import { TaskManagerService } from "../../services/task-manager.service";
     styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
-    // list of workflows with errors
-    errors = [];
+    
+    errors = [];  // list of workflows with errors
+    configSetSub: Subscription;
+    envName: string = "";
 
-    constructor(private auth: AuthService, private taskMgr: TaskManagerService, private router: Router) {}
+    constructor(
+        private auth: AuthService,
+        private taskMgr: TaskManagerService,
+        private router: Router,
+        private envService: EnvService
+    ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.configSetSub = this.envService.configSetObservable.subscribe(configSet => {
+          if (configSet === true) {
+            this.envName = this.envService.config.envName;
+          }
+        });
+    }
 
     logout(): void {
         // logs user out
