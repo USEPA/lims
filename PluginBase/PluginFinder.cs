f3261a31740eb5888d96ad562499e740ea7520b2
Kurt Wolfe <kurt.wolfe@gmail.com>
2025-05-13 08:54:13 -0400

    Additional error handling for plugins

--- PluginBase/PluginFinder.cs
+++ PluginBase/PluginFinder.cs
@@ -22,20 +22,32 @@ namespace PluginBase
         private IReadOnlyCollection<string> FindPluginsInAssemblies(string pluginsPath, string[] assemblyPaths)
         {
             var assemblyPluginInfos = new List<string>();
-            //var pluginFinderAssemblyContext = new PluginAssemblyLoadingContext("PluginFinderAssemblyContext");
-            var pluginFinderAssemblyContext = new CollectibleAssemblyLoadContext(pluginsPath);
-            //var pluginFinderAssemblyContext = new AssemblyLoadContext(name: "PluginFinderAssemblyContext"); 
-            var cwd = System.IO.Directory.GetCurrentDirectory();
-            foreach (var assemblyPath in assemblyPaths)
-            {
-                var fullAssemblyPath = System.IO.Path.Combine(cwd, assemblyPath);
-                var assembly = pluginFinderAssemblyContext.LoadFromAssemblyPath(fullAssemblyPath);
-                if (GetPluginTypes(assembly).Any())
+            string fullAssemblyPath = null;
+            try
+            {                
+                //var pluginFinderAssemblyContext = new PluginAssemblyLoadingContext("PluginFinderAssemblyContext");
+                var pluginFinderAssemblyContext = new CollectibleAssemblyLoadContext(pluginsPath);
+                //var pluginFinderAssemblyContext = new AssemblyLoadContext(name: "PluginFinderAssemblyContext"); 
+                var cwd = System.IO.Directory.GetCurrentDirectory();
+                foreach (var assemblyPath in assemblyPaths)
                 {
-                    assemblyPluginInfos.Add(assembly.Location);
+                    fullAssemblyPath = System.IO.Path.Combine(cwd, assemblyPath);
+                    var assembly = pluginFinderAssemblyContext.LoadFromAssemblyPath(fullAssemblyPath);
+                    if (GetPluginTypes(assembly).Any())
+                    {
+                        assemblyPluginInfos.Add(assembly.Location);
+                    }
                 }
+                pluginFinderAssemblyContext.Unload();
             }
-            pluginFinderAssemblyContext.Unload();
+            catch (Exception ex)
+            {
+                LogMessage(ex.Message + " " + fullAssemblyPath);                                
+            }
+            finally
+            {
+                
+            }
             return assemblyPluginInfos;
         }
 
@@ -46,6 +58,30 @@ namespace PluginBase
                             !type.IsAbstract &&
                             typeof(DataProcessor).IsAssignableFrom(type))
                             .ToArray();
+        }
+
+        private void LogMessage(string message)
+        {
+            string logPath = "";            
+            try
+            {
+                string loc = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                FileInfo fi = new FileInfo(loc);
+                DirectoryInfo di = fi.Directory;
+                logPath = Path.Combine(fi.Directory.FullName, "logs");
+                if (!Directory.Exists(logPath))
+                    Directory.CreateDirectory(logPath);
+
+                logPath = Path.Combine(logPath, "lims_desktop.log");
+
+                File.AppendAllText(logPath, message);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            return;
         }
     }
 }
